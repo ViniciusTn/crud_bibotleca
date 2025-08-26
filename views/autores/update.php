c293b162eec77e5b7ca8673cd7d74b33f02b0e22
ViniciusTn <vinicius_t_nunes@estudante.sesisenai.org.br>
2025-08-26 12:16:30 -0300

    update

---
+++ views/autores/update.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar Autor</title>
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+<body>
+    <h1>Editar Autor</h1>
+    <form id="update-author-form">
+        <input type="hidden" id="id_autor" name="id_autor">
+        
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
+        
+        <label for
+question>I notice the content was cut off. Should I continue with the update.php file for autores?</question>
+</ask_followup_question>
